4491adbbe7f1fdda4bcc65050a52e5d8125f623f
lukagrubisic <luka.grubisic@stud.fh-campuswien.ac.at>
2022-03-14 19:36:36 +0100

    Check Digit

--- src/main/java/PasswordCtrl.java
+++ src/main/java/PasswordCtrl.java
@@ -1,6 +1,38 @@
 public class PasswordCtrl {
-    public static boolean checkPassword(String password) {
-        return password.length() >= 8 && password.length() <= 25;
+
+    public static boolean containsUpperCaseCharacter(String string) {
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isUpperCase(string.charAt(i))){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean containsLowerCaseCharacter(String string){
+        for (int i = 0; i < string.length(); i++) {
+            if(Character.isLowerCase(string.charAt(i))){
+                return true;
+            }
+        }
+        return false;
     }
 
-}
+
+    public static boolean checkPassword(String password) {
+        //if(password.length() >= 8 && password.length() <= 25){
+           // return true;
+        return checkLength(password) && containsLowerCaseCharacter(password)
+                && containsUpperCaseCharacter(password)
+                && checkDigit(password);
+
+    }
+    public static boolean checkLength(String password){
+        return password.length() >= 8 && password.length() <= 25;
+    }
+    public static boolean checkDigit(String input){
+        for (char chr : input.toCharArray())
+             if(Character.isDigit(chr))
+                 return true;
+        return false;
+        }
+    }
--- src/test/java/PasswordCtrlTest.java
+++ src/test/java/PasswordCtrlTest.java
@@ -7,41 +7,67 @@ public class PasswordCtrlTest {
 
     @Test
     void checkPassword_too_short(){
-        boolean actual = PasswordCtrl.checkPassword("1234567");
+        boolean actual = PasswordCtrl.checkLength("1234567");
         boolean expected = false;
         assertEquals(expected, actual);
     }
 
     @Test
     void checkPassword_correct_length(){
-        boolean actual = PasswordCtrl.checkPassword("12345678");
+        boolean actual = PasswordCtrl.checkLength("123456789");
         boolean expected = true;
         assertEquals(expected, actual);
 
     }
     @Test
     void checkPassword_too_long(){
-        boolean actual = PasswordCtrl.checkPassword("123456789012345689012345678901234512345");
+        boolean actual = PasswordCtrl.checkLength("123456789012345689012345678901234512345");
         boolean expected = false;
         assertEquals(expected, actual);
     }
     @Test
     void checkPassword_contains_only_uppercase(){
-        boolean actual = PasswordCtrl.checkPassword("ABCDEFGHIJKLMNO");
-        boolean expected = false;
+        boolean actual = PasswordCtrl.containsUpperCaseCharacter("ABCDEFGHIJKLMNO");
+        boolean expected = true;
         assertEquals(expected, actual);
     }
     @Test
     void checkPassword_contains_only_lowercase(){
-        boolean actual = PasswordCtrl.checkPassword("abcdefghijklmn");
+        boolean actual = PasswordCtrl.containsLowerCaseCharacter("abcdefghijklmn");
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+    @Test
+    void checkPassword_contains_lowercase_false() {
+        boolean actual = PasswordCtrl.containsLowerCaseCharacter("ABCDEFGHIJKLMNO");
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void checkPassword_contains_only_uppercase_false(){
+        boolean actual = PasswordCtrl.containsUpperCaseCharacter("abcdefghijklmno");
+        boolean expected = false;
+        assertEquals(expected, actual);
+    /*@Test - nicht mehr notwendig
+    void checkPassword_contains_lowercase_and_uppercase(){
+        boolean actual = PasswordCtrl.checkPassword("Hallowelten");
+        boolean expected = true;
+        assertEquals(expected, actual);*/
+    }
+
+    @Test
+    void checkPassword_no_numbers(){
+        boolean actual = PasswordCtrl.checkPassword("Halloweltenabc");
         boolean expected = false;
         assertEquals(expected, actual);
     }
     @Test
-    void checkPassword_contains_lowercase_and_uppercase(){
-        boolean actual = PasswordCtrl.checkPassword("Hallowelten");
+    void checkPassword_contains_numbers(){
+        boolean actual = PasswordCtrl.checkPassword("Hallowelten123");
         boolean expected = true;
         assertEquals(expected, actual);
+
     }
 
 }
